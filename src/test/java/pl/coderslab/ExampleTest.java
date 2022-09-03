ec4f3c57c791386ee28ef1b939cdd810d7da60d3
Jacek-Kuczynski <j.kuczy88@gmail.com>
2022-09-03 10:30:53 +0200

    Example Test

---
+++ src/test/java/pl/coderslab/ExampleTest.java
@@ -0,0 +1,12 @@
+package pl.coderslab;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest(){
+        assertEquals(true, true);
+    }
+}
